ec52a301e6e121a938e6014fb33f9270b330b0f7
Ciaran Toner <58338331+Ciaran-io@users.noreply.github.com>
2021-08-15 13:49:20 +0100

    Added attribute data binding

--- Intro-to-Vue-3/main.js
+++ Intro-to-Vue-3/main.js
@@ -2,6 +2,8 @@ const app = Vue.createApp({
   data() {
     return {
       description: 'Cotton socks',
+      image: './assets/images/socks_blue.jpg',
+      url: './assets/images/socks_blue.jpg',
       product: 'Socks',
     };
   },
