79a480c43a0c042330933104b45574a073fedfb6
Ciaran Toner <58338331+Ciaran-io@users.noreply.github.com>
2021-08-15 14:41:05 +0100

    Added list rendering.

--- Intro-to-Vue-3/main.js
+++ Intro-to-Vue-3/main.js
@@ -1,12 +1,26 @@
 const app = Vue.createApp({
   data() {
     return {
-      description: 'Cotton socks',
+      description: 'Preimum socks',
       image: './assets/images/socks_blue.jpg',
       url: './assets/images/socks_blue.jpg',
       product: 'Socks',
       inventory: 20,
       onSale: true,
+      details: ['50% cotton', '30% woool'],
+      sizes: ['small', 'medium'],
+      variants: [
+        {
+          id: 244,
+          color: 'green',
+          image: './assets/images/socks_green.jpg',
+        },
+        {
+          id: 744,
+          color: 'blue',
+          image: './assets/images/socks_blue.jpg',
+        },
+      ],
     };
   },
 });
