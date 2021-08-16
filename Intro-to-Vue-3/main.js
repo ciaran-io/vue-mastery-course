04c71910ab89f9a3e1d4db55b6726130e2883d1b
Ciaran Toner <58338331+Ciaran-io@users.noreply.github.com>
2021-08-16 13:45:20 +0100

    Added computed properites for sales items.

--- Intro-to-Vue-3/main.js
+++ Intro-to-Vue-3/main.js
@@ -3,24 +3,28 @@ const app = Vue.createApp({
     return {
       cart: 0,
       description: 'Preimum socks',
-      image: './assets/images/socks_blue.jpg',
+      brand: 'Vue Mastery',
       url: './assets/images/socks_blue.jpg',
       product: 'Socks',
-      onSale: true,
+      selectedVariant: 0,
+      onSale: false,
+      salesMessage: '',
       details: ['50% cotton', '30% woool'],
-      inStock: true,
       sizes: ['small', 'medium'],
-      inventory: 20,
       variants: [
         {
           id: 244,
           color: 'green',
           image: './assets/images/socks_green.jpg',
+          quantity: 20,
+          onSale: true,
         },
         {
           id: 744,
           color: 'blue',
           image: './assets/images/socks_blue.jpg',
+          quantity: 0,
+          onSale: false,
         },
       ],
     };
@@ -32,8 +36,25 @@ const app = Vue.createApp({
     removeFromCart() {
       this.cart -= 1;
     },
-    updateImage(variantImage) {
-      this.image = variantImage;
+    updateVariant(index) {
+      this.selectedVariant = index;
+      console.log(index);
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+    isOnSale() {
+      this.salesMessage = this.brand + this.product + ' is on sale';
+      if (this.variants[this.selectedVariant].onSale) 
+      return this.salesMessage
     },
   },
 });
